3683f0bf2acf4b24e4462e6997226f84fe578bdc
FiraolMohammed <firaolibnmohammed@gmail.com>
2022-12-29 20:32:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entrey point
+ * print - "Programming is like building a multilingual
+ * puzzle, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzel");
-	return(0);
+	return (0);
 }
-
